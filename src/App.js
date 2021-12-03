09cfd45ed6820bfbce8a14a3262115b808b7074f
unknown <bomix20@gmail.com>
2021-12-03 12:25:16 +0100

    ;)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Formulaire from './new';
+
+function App() {
+  return (
+    <div>
+      <Formulaire/>
+    </div>
+  );
+}
+
+export default App;
